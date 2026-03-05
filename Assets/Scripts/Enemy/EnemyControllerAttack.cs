c00d0fc1da59272033d11117adb8a74eae6cb7e4
slo228555-oss <slo228555@gmail.com>
2026-03-05 19:29:19 +0700

    дороботал врага атака dorobotal vraga ataku

--- Assets/Scripts/Enemy/EnemyControllerAttack.cs
+++ Assets/Scripts/Enemy/EnemyControllerAttack.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
-
+using System.Collections;
 public class EnemyControllerAttack : MonoBehaviour
 {
+
+    [SerializeField] GameObject Player;
+    [SerializeField] GameObject RightHitbox;
+    [SerializeField] GameObject LeftHitbox;
+    private float playerPos ;
+    private float enemyPos ;
+private bool canAttack = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +18,43 @@ public class EnemyControllerAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        playerPos= Player.transform.position.x;
+        enemyPos= transform.position.x;
+        EnemyAttack();
     }
+    public void EnemyAttack()
+    {
+        if( enemyPos > playerPos && canAttack == true)
+        {
+            StartCoroutine(WAitingAttackLeft());
+        }
+        else if (enemyPos < playerPos &&canAttack == true)
+        {
+             StartCoroutine(WAitingAttackRight());
+        }
+    }
+
+
+IEnumerator WAitingAttackLeft()
+    {
+       
+        canAttack = false;
+LeftHitbox.SetActive(true);
+        yield return new WaitForSecondsRealtime(1);
+         LeftHitbox.SetActive(false);
+         yield return new WaitForSecondsRealtime(2);
+         canAttack = true;
+    }
+IEnumerator WAitingAttackRight()
+    {
+       
+        canAttack = false;
+        RightHitbox.SetActive(true);
+        yield return new WaitForSecondsRealtime(1);
+         RightHitbox.SetActive(false);
+         yield return new WaitForSecondsRealtime(2);
+         canAttack = true;
+    }
+
+
 }
